3b4def1a4da81ba60d7ebe480d581e918de86cee
Arseny <a.yanchenko@enot.ai>
2024-11-21 00:17:59 +0100

    feat: ftplugin - toml

--- nixvim/settings.nix
+++ nixvim/settings.nix
@@ -30,6 +30,13 @@
           softtabstop = 2;
         };
       };
+      "ftplugin/toml.lua" = {
+        opts = {
+          shiftwidth = 2;
+          tabstop = 2;
+          softtabstop = 2;
+        };
+      };
     };
   };
 }
